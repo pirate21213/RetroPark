0137f880ec14d5c07f44456b9a94e3e650934a3b
pirate21213 <pirate21213@gmail.com>
2021-09-03 20:35:40 -0500

    Somewhat functional cv2 approach, its not able to properly scale the output image however

--- image_cutter_cv2.py
+++ image_cutter_cv2.py
@@ -1,3 +1,4 @@
+# Made following https://jdhao.github.io/2019/02/23/crop_rotated_rectangle_opencv/
 import numpy as np
 import cv2
 
@@ -10,3 +11,25 @@ bound = np.array([               # These coordinates are what we would need to c
             [[1616, 2034]]
         ])
 print("Shape of bound: {}".format(bound.shape))
+rect = cv2.minAreaRect(bound)
+print("rect: {}".format(rect))
+
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+
+print("bounding box: {}".format(box))
+cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+width = int(rect[1][0])
+height = int(rect[1][1])
+
+src_pts = box.astype("float32")
+
+dst_pts = np.array([[0, width-1],
+                   [0, 0],
+                   [height-1, 0],
+                   [height-1, width-1]], dtype="float32")
+
+M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+warped = cv2.warpPerspective(img, M, (width, height))
+cv2.imwrite("test_output.JPG", warped)
