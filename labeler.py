6ebc1223ad8ec1dc2963032e97c69bd5325f6612
pirate21213 <pirate21213@gmail.com>
2021-09-28 11:38:14 -0500

    update labeler.py to label output images with their spotID

--- labeler.py
+++ labeler.py
@@ -6,6 +6,7 @@ import os
 import cv2
 import argparse
 from datetime import datetime
+import re
 
 # Set up argparse to input the image file location
 ap = argparse.ArgumentParser()
@@ -22,30 +23,27 @@ if not os.path.exists(os.path.join(args["output_folder"], "nocc")):
     os.makedirs(nocc)
 
 images = []
-
-scale_percent = 30  # percent of original size
-images = [cv2.imread(file) for file in glob.glob("{}*.jpg".format(args["input_folder"]))]
+spot_ID = [] # Pulls the spotID from the image name for later label use
+for file in glob.glob("{}*.jpg".format(args["input_folder"])):
+    images.append(cv2.imread(file))
+    p = re.findall(r'\d+', str(file))
+    spot_ID.append(p[0])
+print(spot_ID)
 
 # Create timestamp
 time = datetime.now().strftime("%H_%M_%S")
-count = 0
 
-for image in images:
+for i in range(0, len(images)):
     while True:
         cv2.namedWindow("[U]noccupied or [O]ccupied")
-        cv2.imshow("[U]noccupied or [O]ccupied", image)
+        cv2.imshow("[U]noccupied or [O]ccupied", images[i])
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             quit()
         if key == ord("u"):
-            cv2.imwrite(os.path.join(nocc, "nocc_%s_%s.jpg" % (time, count)), image)
-            count += 1
+            cv2.imwrite(os.path.join(nocc, "nocc_%s_%s.jpg" % (spot_ID[i], time)), images[i])
             break
         if key == ord("o"):
-            cv2.imwrite(os.path.join(occ, "occ_%s_%s.jpg" % (time, count)), image)
-            count += 1
+            cv2.imwrite(os.path.join(occ, "occ_%s_%s.jpg" % (spot_ID[i], time)), images[i])
             break
-
-
-
