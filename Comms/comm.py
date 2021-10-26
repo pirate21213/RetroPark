76e45a7728d4c01f986a02fe20a73d627684be65
pirate21213 <pirate21213@gmail.com>
2021-10-26 10:50:01 -0500

    updates

--- Comms/comm.py
+++ Comms/comm.py
@@ -85,6 +85,8 @@ def receive_file(save_path):
     # and writing to the file stream
     progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     print("Opening file locally at {}".format(out_filename))
+
+    # populate data
     with open(out_filename, "wb+") as f:
         while True:
             # read 1024 bytes from the socket (receive)
